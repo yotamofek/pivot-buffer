c855cde0d5d49c0e7a81dd38787f9871fbbaf35e
Yotam Ofek <yotam.ofek@gmail.com>
2015-12-19 20:44:50 +0200

    Added jsdoc documentation.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 'use strict';
 
-module.exports = class PivotBuffer {
+/**
+ * Rotating data buffer for Node.js
+ */
+class PivotBuffer {
+    /**
+     * @param {number} size - The fixed size of the buffer, beyond which point data will be rotated.
+     */
     constructor(size) {
         this.size = size;
         this.$buffer = new Buffer(size);
@@ -9,7 +15,12 @@ module.exports = class PivotBuffer {
 
         this.$curSize = 0;
     }
-    
+
+    /**
+     * Append a node buffer to the current buffer, rotating the current data if necessary.
+     * If buffer to append is bigger than the cap of the pivot buffer, only the last portion of it will be written.
+     * @param {Buffer} buffer - the buffer to append
+     */
     append(buffer) {
         if (buffer.length === 0) {
             return;
@@ -30,13 +41,23 @@ module.exports = class PivotBuffer {
         this.$curSize = Math.min(this.size, this.$curSize + buffer.length);
     }
 
+    /**
+     * The underlying buffer. Truncated to size of current data (may be smaller than size specified in constructor).
+     * @return {Buffer}
+     */
     get value() {
         return this.$curSize != this.size
             ? this.$buffer.slice(this.size - this.$curSize) // truncate the buffer if it was not yet filled up
             : this.$buffer;
     }
 
+    /**
+     * Length of current buffer, between the range of 0 to buffer size, inclusive.
+     * @return {number}
+     */
     get length() {
         return this.$curSize;
     }
-};
+}
+
+module.exports = PivotBuffer;
